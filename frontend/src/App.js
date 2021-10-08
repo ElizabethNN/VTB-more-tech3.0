10575995b0dd3b94baeb2a1b22db30215d190c80
Can4es322 <66887951+Can4es322@users.noreply.github.com>
2021-10-08 20:25:16 +0300

    button-route
    тестируем роут

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import AuthorizationPage from './AuthorizationPageComponents/AuthorizationPage';
+import MainPage from "./ComponentsMainPage/MainPage";
 
 function App() {
   return (
     <div className="App">
-      <AuthorizationPage/>
+      <AuthorizationPage></AuthorizationPage>
     </div>
   );
 }
---
+++ frontend/src/ComponentsMainPage/MainPage.js
@@ -0,0 +1,12 @@
+import { Button } from '@mui/material';
+
+function MainPage() {
+    return (
+        <div>
+            <Button  href="#text-buttons">Доступные данные</Button>
+            <Button>Создание фичи</Button>
+        </div>
+    );
+}
+
+export default MainPage;
