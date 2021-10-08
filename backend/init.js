653d52caf2f9dc1486fea6a64ca4c03b801e635d
Gataullin Ruslan <rusikarm3@gmail.com>
2021-10-08 21:37:38 +0300

    init file refactoring

--- backend/init.js
+++ backend/init.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const { auth } = require('express-openid-connect');
-
+const cors = require('cors');
 const app = express();
 
-//TODO: Add CORS
+// TODO: make more secure
+var corsOption = {
+    origin: '*'
+}
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use((err, req, res, next)=>{
+app.use(cors(corsOption));
+
+app.use((err, req, res, next) => {
     console.error(err);
-    if(!res.status) res.send(500, "Something went wrong");
+    if (!res.status) res.send(500, "Something went wrong");
     else next();
 })
 
@@ -18,12 +23,19 @@ require('dotenv').config()
 app.env = process.env;
 
 if (app.env.DEBUG){
-    app.use((req, res, next)=>{
-        console.log({"url":req.originalUrl, "method": req.method, "headers":req.headers, "body":req.body, "oidc":req.oidc});
+    app.use((req, res, next) => {
+        console.log({
+                "url": req.originalUrl, 
+                "method": req.method, 
+                "headers":req.headers, 
+                "body":req.body, 
+                "oidc":req.oidc
+            });
         next();
     })
 }
 
+// auth config
 const config = { 
     authRequired: false,
     auth0Logout: true,
--- backend/settingRoutes.js
+++ backend/settingRoutes.js
@@ -1,1 +1,4 @@
 const app = require("./init.js");
+
+// TODO: const module = require('./module/path);
+//       app.use('api/path', module);
