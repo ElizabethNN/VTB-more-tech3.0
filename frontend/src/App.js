a1a0a5beca686c8ad4fc86a2393f6b1815075d39
StadndartAccount <72969531+StadndartAccount@users.noreply.github.com>
2021-10-08 19:45:55 +0300

    обновил material ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import AuthorizationPage from './AuthorizationPageComponents/AuthorizationPage';
 
 function App() {
   return (
     <div className="App">
-
+      <AuthorizationPage/>
     </div>
   );
 }
---
+++ frontend/src/AuthorizationPageComponents/AuthorizationPage.js
@@ -0,0 +1,12 @@
+//import './AuthorizationPage.css';
+import { Button } from '@mui/material';
+
+function AuthorizationPage() {
+  return (
+    <div className="AuthorizationPage">
+        <Button variant="contained">Contained</Button>
+    </div>
+  );
+}
+
+export default AuthorizationPage;
