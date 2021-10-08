753906ff93413d4211b916b48c8ba932cdc6bb84
Can4es322 <66887951+Can4es322@users.noreply.github.com>
2021-10-09 02:14:43 +0300

    фбобф

--- frontend/src/AvailableDataPage/AvailableData.js
+++ frontend/src/AvailableDataPage/AvailableData.js
@@ -1,24 +1,12 @@
 import * as React from "react";
-import Paper from "@mui/material/Paper";
-import InputBase from "@mui/material/InputBase";
-import IconButton from "@mui/material/IconButton";
-import { SearchOutlined } from "@material-ui/icons";
 import { Pagination } from "@mui/material";
 import InfoCard from "./InfoCard/InfoCard";
-import { Grid } from "@mui/material";
+import SearchInput from "../SearchInput/SearchInput";
 
 function AvailableData() {
   return (
     <>
-      <Paper
-        component="form"
-        sx={{ p: "2px 4px", display: "flex", alignItems: "center" }}
-      >
-        <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Поиск" />
-        <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
-          <SearchOutlined />
-        </IconButton>
-      </Paper>
+        <SearchInput/>
       <InfoCard />
       <InfoCard />
       <InfoCard />
