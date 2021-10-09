3a20ccbffdfbac64cdf64af29078c6f2adf02cfe
StadndartAccount <72969531+StadndartAccount@users.noreply.github.com>
2021-10-09 17:01:00 +0300

    отступы

--- frontend/src/AuthorizationPage/Authorization.js
+++ frontend/src/AuthorizationPage/Authorization.js
@@ -3,10 +3,10 @@ import s from "./Authorization.module.css";
 
 function Authorization() {
   return (
-    <div className="Authorization" style={{display: "flex", flexDirection: "column", padding: "20%"}}>
-      <TextField className={s.item} id="outlined-basic" label="Логин" variant="outlined" />
-      <TextField className={s.item} id="outlined-basic" label="Пароль" variant="outlined" type="password" />
-      <Button className={s.item} variant="contained">Авторизация</Button>
+    <div style={{display: "flex", flexDirection: "column", padding: "20%"}}>
+      <TextField  id="outlined-basic" label="Логин" variant="outlined" className={s.item}/>
+      <TextField  id="outlined-basic" label="Пароль" variant="outlined" type="password" className={s.item} />
+      <Button variant="contained" className={s.item} size="large">Авторизация</Button>
     </div>
   );
 }
