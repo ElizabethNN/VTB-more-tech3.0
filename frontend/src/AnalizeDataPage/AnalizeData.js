c7cf90c0c6e89a4329ad286a699fe751ae1e4e56
Can4es322 <66887951+Can4es322@users.noreply.github.com>
2021-10-09 13:31:16 +0300

    анализ данных flex

--- frontend/src/AnalizeDataPage/AnalizeData.js
+++ frontend/src/AnalizeDataPage/AnalizeData.js
@@ -1,34 +1,21 @@
-import { Grid, Box } from "@mui/material";
 import s from "./AnalizeData.module.css";
 import CardAnalize from "./../CardAnalize/CardAnalize";
 
 function AnalizeData() {
   return (
-    <Grid
-      container
-      direction="column"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Grid>Name</Grid>
-      <div className={s.container}></div>
-      <Grid
-        container
-        item
-        direction="row"
-        justifyContent="space-between"
-        alignItems="center"
-      >
-        <Grid item xs={6}>
-          <Box sx={{display: "flex", bgcolor: 'primary.dark', height:"100%"}}/>
-        </Grid>
-        <Grid item xs={6} md={5}>
-          <div>
-            <CardAnalize />
-          </div>
-        </Grid>
-      </Grid>
-    </Grid>
+
+    <div className={s.container}>
+      <div className={s.nameData}>Name</div>
+
+      <div className={s.content}>
+        <div className={s.graphic}>
+           ffdffd
+        </div>
+        <div className={s.infoData}>
+          <CardAnalize />
+        </div>
+      </div>
+    </div>
   );
 }
 
