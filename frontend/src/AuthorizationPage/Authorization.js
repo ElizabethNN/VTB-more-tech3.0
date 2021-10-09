142cc9dd3b11980ba573c1fabc6eb401b9e7fab1
StadndartAccount <72969531+StadndartAccount@users.noreply.github.com>
2021-10-09 20:48:46 +0300

    авторизаия

--- frontend/src/AuthorizationPage/Authorization.js
+++ frontend/src/AuthorizationPage/Authorization.js
@@ -4,9 +4,9 @@ import s from "./Authorization.module.css";
 function Authorization() {
   return (
     <div style={{display: "flex", flexDirection: "column", padding: "20%"}}>
-      <TextField  id="outlined-basic" label="Логин" variant="outlined" className={s.item}/>
-      <TextField  id="outlined-basic" label="Пароль" variant="outlined" type="password" className={s.item} />
-      <Button variant="contained" className={s.item} size="large">Авторизация</Button>
+      <TextField  id="outlined-basic" label="Логин" variant="outlined" sx={{my:1}}/>
+      <TextField  id="outlined-basic" label="Пароль" variant="outlined" type="password" sx={{my:1}} />
+      <Button variant="contained" sx={{my:1}} size="large">Авторизация</Button>
     </div>
   );
 }
